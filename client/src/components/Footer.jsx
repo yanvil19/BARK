579b07356d4dde46b553ddfe9ca436438594ae23
PigZahHot <coliva358@gmail.com>
2026-04-16 19:32:57 +0800

    Added Navbar And Footer with CSS

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+import '../styles/Footer.css';
+
+export default function Footer() {
+  return (
+    <footer id="Footer">
+      <p>© 2026 NU-BOARD | National University Laguna</p>
+    </footer>
+  );
+}
